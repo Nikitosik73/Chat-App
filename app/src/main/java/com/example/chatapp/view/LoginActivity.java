9251a24da189a6280a6cc4b2a72ab163748d53e7
nikita <paramonov20@bk.ru>
2023-02-19 03:10:46 +0300

    LoginViewModel

--- app/src/main/java/com/example/chatapp/view/LoginActivity.java
+++ app/src/main/java/com/example/chatapp/view/LoginActivity.java
@@ -2,16 +2,23 @@ package com.example.chatapp.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.chatapp.databinding.ActivityLoginBinding;
+import com.example.chatapp.viewmodel.LoginViewModel;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
+
+    private LoginViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,34 +27,19 @@ public class LoginActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+        observeViewModel();
+        setOnListener();
+    }
+
+    private void setOnListener(){
         binding.buttonEnter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = binding.editTextInputEmail.getText().toString().trim();
                 String password = binding.editTextInputPassword.getText().toString().trim();
-                if (email.isEmpty() && password.isEmpty()){
-                    Toast.makeText(
-                            LoginActivity.this,
-                            "Вы не заполнили не одного поля",
-                            Toast.LENGTH_SHORT
-                    ).show();
-                } else if (email.isEmpty()) {
-                    Toast.makeText(
-                            LoginActivity.this,
-                            "Заполните поле email!!!",
-                            Toast.LENGTH_SHORT
-                    ).show();
-                }else if (password.isEmpty()) {
-                    Toast.makeText(
-                            LoginActivity.this,
-                            "Заполните поле password!!!",
-                            Toast.LENGTH_SHORT
-                    ).show();
-                } else {
-                    Intent intent = ChatActivity.newIntent(LoginActivity.this);
-                    startActivity(intent);
-                }
                 // login
+                viewModel.login(email, password);
             }
         });
         binding.textViewForgotPassword.setOnClickListener(new View.OnClickListener() {
@@ -56,7 +48,7 @@ public class LoginActivity extends AppCompatActivity {
                 Intent intent = ForgotPasswordActivity.newIntent(
                         LoginActivity.this,
                         binding.editTextInputEmail.getText().toString().trim()
-                        );
+                );
                 startActivity(intent);
             }
         });
@@ -65,6 +57,30 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = RegistrationActivity.newIntent(LoginActivity.this);
                 startActivity(intent);
+            }
+        });
+    }
+
+    private void observeViewModel() {
+        viewModel.getError().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String error) {
+                Toast.makeText(
+                        LoginActivity.this,
+                        error,
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+
+        viewModel.getUser().observe(this, new Observer<FirebaseUser>() {
+            @Override
+            public void onChanged(FirebaseUser firebaseUser) {
+                if (firebaseUser != null) {
+                    Intent intent = ChatActivity.newIntent(LoginActivity.this);
+                    startActivity(intent);
+                    finish();
+                }
             }
         });
     }
--- app/src/main/java/com/example/chatapp/viewmodel/LoginViewModel.java
+++ app/src/main/java/com/example/chatapp/viewmodel/LoginViewModel.java
@@ -1,13 +1,66 @@
 package com.example.chatapp.viewmodel;
 
 import android.app.Application;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.chatapp.view.LoginActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginViewModel extends AndroidViewModel {
 
+    private FirebaseAuth auth;
+
+    private MutableLiveData<String> error = new MutableLiveData<>();
+
+    private MutableLiveData<FirebaseUser> user = new MutableLiveData<>();
+
     public LoginViewModel(@NonNull Application application) {
         super(application);
+        auth = FirebaseAuth.getInstance();
+        auth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    user.setValue(firebaseAuth.getCurrentUser());
+                }
+            }
+        });
+    }
+
+    public LiveData<String> getError() {
+        return error;
+    }
+
+    public LiveData<FirebaseUser> getUser() {
+        return user;
+    }
+
+    public void login(String email, String password){
+
+        if (!email.isEmpty() && !password.isEmpty()){
+            auth.signInWithEmailAndPassword(email, password)
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            error.setValue(e.getMessage());
+                        }
+                    });
+
+        }
     }
 }
